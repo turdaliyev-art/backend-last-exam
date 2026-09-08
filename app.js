04ad841dc09da77d9af21dda2c0ab13924dd1f4c
turdaliyev-art <bturdaliyev49@gmail.com>
2026-09-08 16:27:37 +0500

    port succesfully

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const venueTypesRoutes = require("./routes/venueTypesRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
